24f213d9933df793cd050dac1b8d0cda68663e0f
abhishekv5055 <abhishek.vaish_int18@gla.ac.in>
2022-01-14 17:37:24 +0530

    Day2

---
+++ Day2/kth_smallest_number.java
@@ -0,0 +1,19 @@
+class Solution{
+    public static int kthSmallest(int[] arr, int l, int r, int k) 
+    {
+        int min = Integer.MAX_VALUE;
+        int index = 0;
+        for(int j = 0; j<k; j++){
+            min = Integer.MAX_VALUE;
+            for(int i = 0; i<arr.length; i++){
+                if(arr[i] < min){
+                    min = arr[i];
+                    index = i;
+                }
+            }
+            arr[index] = Integer.MAX_VALUE;
+        }
+        
+        return min;
+    } 
+}
