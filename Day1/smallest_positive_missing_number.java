565c9c0e3da5cc0ca9edbbc9422d5deef07ba69c
abhishekv5055 <abhishek.vaish_int18@gla.ac.in>
2022-01-14 15:16:56 +0530

    add link

--- Day1/smallest_positive_missing_number.java
+++ Day1/smallest_positive_missing_number.java
@@ -1,12 +1,14 @@
-class Solution
-{
-    //Function to find the smallest positive number missing from the array.
-    static int missingNumber(int arr[], int size)
-    {
+
+//https://practice.geeksforgeeks.org/problems/smallest-positive-missing-number-1587115621/1/?category[]=Arrays&category[]=Mathematical&category[]=Arrays&category[]=Mathematical&difficulty[]=1&page=2&query=category[]Arrayscategory[]Mathematicaldifficulty[]1page2category[]Arrayscategory[]Mathematical#
+import java.util.*;
+
+class Solution {
+    // Function to find the smallest positive number missing from the array.
+    static int missingNumber(int arr[], int size) {
         int sum = 1;
         Arrays.sort(arr);
-        for(int i = 0; i<size; i++){
-            if(arr[i] == sum){
+        for (int i = 0; i < size; i++) {
+            if (arr[i] == sum) {
                 sum++;
             }
         }
