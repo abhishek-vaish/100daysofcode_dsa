f8002f1a055fbc8d433a0b66ec96b71736bc6a65
abhishekv5055 <abhishek.vaish_int18@gla.ac.in>
2022-01-16 13:48:08 +0530

    Day2

---
+++ Day2/find_duplicate_number.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++)
+            max = Math.max(max, nums[i]);
+
+        int[] counter = new int[max];
+
+        for (int i = 0; i < nums.length; i++)
+            counter[nums[i] - 1]++;
+
+        int ans = 0;
+
+        for (int i = 0; i < counter.length; i++) {
+            if (counter[i] > 1) {
+                ans = i + 1;
+            }
+        }
+
+        return ans;
+    }
+}
